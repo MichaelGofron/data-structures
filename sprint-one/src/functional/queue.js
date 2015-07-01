1e531f1a9c21e1ee6b63abab8c8ca311ad724ea6
Mychael Zuniga <mychael.zuniga@yahoo.com>
2015-07-01 14:37:51 -0700

    finish queue and stack functional instantiation

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,26 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var length = 0;
+  var head = 0;
 
   // Implement the methods below
+  // size of queue === length - head
 
-  someInstance.enqueue = function(value){
+  someInstance.enqueue = function(value) {
+    storage[length] = value;
+    length++;
   };
 
   someInstance.dequeue = function(){
+    if ((length - head) > 0) { // 
+      head++;
+    }
+    return storage[head - 1];
   };
 
   someInstance.size = function(){
+    return length - head;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,26 @@
 var Stack = function(){
   var someInstance = {};
+  var length = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
-  someInstance.push = function(value){
+  someInstance.push = function(value) {
+    storage[length] = value;
+    length++;
   };
 
   someInstance.pop = function(){
+    if (length > 0) {
+      length--;
+    }
+
+    return storage[length];
   };
 
   someInstance.size = function(){
+    return length;
   };
 
   return someInstance;
