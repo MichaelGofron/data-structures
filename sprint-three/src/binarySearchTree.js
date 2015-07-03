33f9e280bc84aa407a06290a5cc414d87a5426f4
Mychael Zuniga <mychael.zuniga@yahoo.com>
2015-07-03 09:23:12 -0700

    add bloomfields and binarySearchTree functionality

--- sprint-three/src/binarySearchTree.js
+++ sprint-three/src/binarySearchTree.js
@@ -1,3 +1,15 @@
+// for console debugging
+
+_ = {};
+_.extend = function (obj) {
+	for (var i = 0; i < arguments.length; i++) {
+		for (var key in arguments[i]) {
+			obj[key] = arguments[i][key];
+		}
+	}
+	return obj;
+};
+
 var BinarySearchTree = function(value){
 	var bst = {};
 
@@ -24,6 +36,7 @@ BinarySearchTree.prototype = {
 			}
 		}
 	},
+
 	contains: function (value) {
 		if (this.value === value) {
 			return true;
@@ -34,6 +47,7 @@ BinarySearchTree.prototype = {
 			return (this.right !== undefined) ? this.right.contains(value) : false;
 		}
 	},
+
 	depthFirstLog: function (cb) {
 		cb(this.value);
 		if (this.left !== undefined){
@@ -43,6 +57,7 @@ BinarySearchTree.prototype = {
 			this.right.depthFirstLog(cb);
 		}
 	},
+
 	breadthFirstLog: function (cb) {
 		var queue = [];
 		var children = [this];
@@ -66,8 +81,89 @@ BinarySearchTree.prototype = {
 			cb(tree);
 		});
 
+	},
+
+	depth: function (level) {
+		if (this === window) { return 0; }
+		level = level + 1 || 1;
+		return Math.max(level, BinarySearchTree.prototype.depth.call(this.left, level), BinarySearchTree.prototype.depth.call(this.right, level));
+	},
+
+	getMaxLeftOrRight: function (side) { 
+		var node = this;
+		while (node[side] !== undefined) {
+			node = node[side];
+		}
+		return node;
+	},
+
+	getMax: function () {
+		return this.getMaxLeftOrRight('right');
+	},
+
+	getMin: function () {
+		return this.getMaxLeftOrRight('left');
+	},
+
+	getParent: function (value) {
+		if (this.left.value === value || this.right.value === value) {
+			return this;
+		}
+		if (this.value > value && this.left !== undefined) {
+			return this.left.getParent(value);
+		}
+
+		if (this.value > value && this.right !== undefined) {
+			return this.right.getParent(value);
+		}
+	},
+
+	rebalance: function () {
+		if ((this.left.depth() / this.right.depth()) >= 2) {
+			var newRoot = this.left.getMax(),
+				newRootParent = this.getParent(newRoot.value);
+			// remove newRoot as a leaf
+			newRootParent.right = undefined;
+
+			// add left and this to newRoot
+			newRoot.left = this.left;
+			newRoot.right = this;
+
+			// remove left from this
+			this.left = undefined;
+
+			if ((newRoot.left.depth() / newRoot.right.depth()) > 1) {
+				newRoot.rebalance();
+			}
+		}
+
+		if ((this.right.depth() / this.left.depth()) >= 2) {
+			var newRoot = this.right.getMin(),
+				newRootParent = this.getParent(newRoot.value);
+
+			// remove newRoot as a leaf
+			newRootParent.left = undefined;
+
+			// add right and this to newRoot
+			newRoot.right = this.right;
+			newRoot.left = this;
+
+			// remove right from this
+			this.right = undefined;
+
+			if ((this.right.depth() / this.left.depth()) > 1) {
+				newRoot.rebalance();
+			}
+		}
 	}
 };
+
+/*
+	 [10]
+   [5     14]
+  [3 8][12   18]
+ 	 [11 13 16	19]	
+*/
 
 
 /*
